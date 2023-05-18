11d383ee3562ea6706c03c00b39ce4b747371ea3
lesenelir <miaomiaobabyzy@gmail.com>
2023-05-18 19:51:22 +0800

    feat: finish static website

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -3,9 +3,28 @@ import Divide from "@/components/utils/Divide"
 function Menu() {
 
   return (
-    <div className={'w-64 bg-amber-200'}>
-      Menu
+    <div className={'flex flex-col justify-between w-64 bg-menuColors-800 p-2'}>
+      {/*Top*/}
+      <div className={'mb-4'}>
+        <button className={'p-2 border rounded-lg mb-2 w-full text-left text-white'}>+ New chat</button>
+        <input
+          type="text"
+          placeholder={'Search...'}
+          className={'border border-gray-300 p-1 rounded-lg w-full focus-visible:outline-none'}
+        />
+      </div>
+
+      {/*Content*/}
       <Divide/>
+      <div className={'flex-grow'}></div>
+
+      {/*Button*/}
+      <Divide/>
+      <div className={'mt-4'}>
+        <button className={'p-2 rounded-lg mb-2 w-full text-left text-white hover:bg-menuColors-700'}>Clear Conversations</button>
+        <button className={'p-2 rounded-lg mb-2 w-full text-left text-white hover:bg-menuColors-700'}>OpenAI key</button>
+        <button className={'p-2 rounded-lg mb-2 w-full text-left text-white hover:bg-menuColors-700'}>Settings</button>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,6 +12,21 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        menuColors: {
+          '50': '#f6f6f7',
+          '100': '#e2e4e5',
+          '200': '#c5c8ca',
+          '300': '#a0a3a8',
+          '400': '#7c8085',
+          '500': '#62656a',
+          '600': '#4d5054',
+          '700': '#404245',
+          '800': '#35383a',
+          '900': '#2f3032',
+          '950': '#202123',
+        }
       },
     },
   },
