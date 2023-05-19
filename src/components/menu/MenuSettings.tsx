e2ef714422aede0c7d8e256d914791215c8ea770
lesenelir <miaomiaobabyzy@gmail.com>
2023-05-19 17:05:55 +0800

    chore: add margin css

--- src/components/menu/MenuSettings.tsx
+++ src/components/menu/MenuSettings.tsx
@@ -12,7 +12,7 @@ function MenuSettings(props: IProps) {
   const {imgSrc, imgAlt, imgWidth, imgHeight, buttonContent} = props
 
   return (
-    <div className={'flex flex-row rounded-lg hover:bg-menuColors-700'}>
+    <div className={'flex flex-row rounded-lg mb-1 hover:bg-menuColors-700'}>
       <Image
         src={imgSrc}
         alt={imgAlt}
