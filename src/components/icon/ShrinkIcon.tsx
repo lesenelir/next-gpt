412dab77038fbd372ecc58e4636fd49dd2eb31ea
lesenelir <miaomiaobabyzy@gmail.com>
2023-05-22 00:51:21 +0800

    chore: add shrink svg component

---
+++ src/components/icon/ShrinkIcon.tsx
@@ -0,0 +1,17 @@
+function ShrinkIcon() {
+  return (
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none"
+           stroke="currentColor"
+           strokeWidth="2" stroke-linecap="round" strokeLinejoin="round" className="feather feather-align-justify">
+        <line x1="21" y1="10" x2="3" y2="10"/>
+        <line x1="21" y1="6" x2="3" y2="6"/>
+        <line x1="21" y1="14" x2="3" y2="14"/>
+        <line x1="21" y1="18" x2="3" y2="18"/>
+      </svg>
+
+    </>
+  )
+}
+
+export default ShrinkIcon
