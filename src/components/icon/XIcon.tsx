bba10faeecc4738750ae00332ba36cc3ae7813d1
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-01 22:41:56 +0800

    chore: code style

--- src/components/icon/XIcon.tsx
+++ src/components/icon/XIcon.tsx
@@ -8,7 +8,6 @@ interface IProps {
 function XIcon(props: IProps) {
   const {width, height, className, onClick} = props
 
-
   return (
     <div className={className} onClick={onClick}>
       <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 24 24" fill="none" stroke="currentColor"
