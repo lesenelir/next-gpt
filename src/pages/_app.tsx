e110d8823e42f91936fb717710938b402c2cebdb
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-05 12:12:46 +0800

    chore: change layout name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import ThemeProvider from "@/components/utils/ThemeProvider"
-import Layout from "@/pages/Layout"
+import Layout from "@/pages/layout"
 
 export default function App({Component, pageProps}: AppProps) {
   return (
