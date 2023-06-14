0a6e7bc56ed8875d85e3d75737d17eb573ad45ce
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-14 20:36:56 +0800

    chore: update underScope2Camel

--- src/libs/underScope2Camel.ts
+++ src/libs/underScope2Camel.ts
@@ -1,3 +1,5 @@
+import {IChat, IChatMessage} from "@/libs/myContext"
+
 export interface IDataFromServer {
   id: number
   user_id: number
@@ -14,38 +16,64 @@ export interface IChatMessageFromServer {
   is_User: boolean
 }
 
-export function underScope2Camel(dataFromServer: IDataFromServer[]) {
-  let res = []
+export function underScope2Camel(dataFromServer: IDataFromServer[]): IChat[] {
+  let res: IChat[] = []
 
   for (let item of dataFromServer) {
-    traversal(item)
-    res.push(item)
+    let newItem: IChat = {
+      id: item.id,
+      userId: item.user_id,
+      itemName: item.item_name,
+      itemUUID: item.item_uuid,
+      modifyDate: item.modify_date,
+      ChatMessage: item.ChatMessage.map((message: IChatMessageFromServer): IChatMessage => {
+        return {
+          id: message.id,
+          chatId: message.chat_id,
+          messageContent: message.message_content,
+          isUser: message.is_User
+        }
+      })
+    }
+    res.push(newItem)
   }
   return res
-
-  function traversal(node: any) { // The node parameter is an object.
-    if (!node) return
-
-    let keys = Object.keys(node)
-    for (let key of keys) {
-      let newKey
-
-      if (key === 'item_uuid') {
-        newKey = 'itemUUID'
-      } else {
-        newKey = key.replace(/_(\w)/g, (match, p1) => p1.toUpperCase())
-      }
-
-      if (key !== newKey) {
-        node[newKey] = node[key]
-        delete node[key]
-      }
-    }
-
-    node.ChatMessage && node.ChatMessage.forEach((item: any) => {
-      traversal(item)
-    })
-  }
 }
+
+
+
+// export function underScope2Camel(dataFromServer: IDataFromServer[]) {
+//   let res = []
+//
+//   for (let item of dataFromServer) {
+//     traversal(item)
+//     res.push(item)
+//   }
+//   return res
+//
+//   function traversal(node: any) { // The node parameter is an object.
+//     if (!node) return
+//
+//     let keys = Object.keys(node)
+//     for (let key of keys) {
+//       let newKey
+//
+//       if (key === 'item_uuid') {
+//         newKey = 'itemUUID'
+//       } else {
+//         newKey = key.replace(/_(\w)/g, (match, p1) => p1.toUpperCase())
+//       }
+//
+//       if (key !== newKey) {
+//         node[newKey] = node[key]
+//         delete node[key]
+//       }
+//     }
+//
+//     node.ChatMessage && node.ChatMessage.forEach((item: any) => {
+//       traversal(item)
+//     })
+//   }
+// }
 
 
