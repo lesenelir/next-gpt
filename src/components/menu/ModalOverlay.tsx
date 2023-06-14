29cf3fc00c0123d81a455f9fa7da0448ea0720a0
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-14 20:40:10 +0800

    chore: change code style

--- src/components/menu/ModalOverlay.tsx
+++ src/components/menu/ModalOverlay.tsx
@@ -11,7 +11,9 @@ function ModalOverlay(props: IProps) {
   return (
     <div className='drop-shadow-2lg fixed z-50 inset-0 bg-black bg-opacity-60 flex items-center justify-center' onClick={onClose}>
       {/* Prevents the modal from closing when clicking on the modal itself */}
-      <div className={'w-1/3 h-1/3 p-2 max-sm:w-11/12 max-sm:h-1/3 max-sm:flex max-sm:flex-col max-sm:justify-center'} onClick={(e: MouseEvent<HTMLDivElement>) => e.stopPropagation()}>
+      <div className={'w-1/3 h-1/3 p-2 max-sm:w-11/12 max-sm:h-1/3 max-sm:flex max-sm:flex-col max-sm:justify-center'}
+           onClick={(e: MouseEvent<HTMLDivElement>) => e.stopPropagation()}
+      >
         {children}
       </div>
     </div>
