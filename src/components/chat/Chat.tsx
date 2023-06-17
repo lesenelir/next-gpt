3a989e16291337258c97af0e70383e6b902b6087
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-18 02:45:38 +0800

    feat: click chat box item to render list

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,7 +1,9 @@
-import {useContext, useState} from "react"
+import {useContext, useEffect, useState} from "react"
 import {useTranslation} from "next-i18next"
+import {useRouter} from "next/router"
 
 import {IChatMessage, MyContext} from "@/libs/myContext"
+import {chatMessageCamel} from "@/libs/underScope2Camel"
 import ChatInput from "@/components/chat/ChatInput"
 import Footer from "@/components/utils/Footer"
 import UserIcon from "@/components/icon/UserIcon"
@@ -9,8 +11,40 @@ import BotIcon from "@/components/icon/BotIcon"
 
 function Chat() {
   const [isChatting, setIsChatting] = useState<boolean>(false)
-  const {theme, chatMessage} = useContext(MyContext)
+  const {theme, chatMessage, setChatMessage} = useContext(MyContext)
   const {t} = useTranslation('common')
+  const router = useRouter()
+
+  useEffect(() => {
+    const init = async () => {
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          chat_uuid: router.query.id
+        })
+      }
+
+      try {
+        const response = await fetch('/api/isChat', options)
+        const data = await response.json()
+        const dataFromBackend = data?.data
+        if (Array.isArray(dataFromBackend)) {
+          setIsChatting(true)
+          setChatMessage(chatMessageCamel(dataFromBackend))
+        } else {
+          setIsChatting(false)
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    init().then(r => console.log(r))
+
+  }, [router.query.id, setChatMessage])
 
   // Render List
   const chatList = chatMessage.map((chat: IChatMessage) => {
---
+++ src/pages/api/isChat.ts
@@ -0,0 +1,31 @@
+import {NextApiRequest, NextApiResponse} from "next"
+import nc from 'next-connect'
+import {ChatMessage, PrismaClient} from '@prisma/client'
+
+const handler = nc<NextApiRequest, NextApiResponse>()
+
+const prisma = new PrismaClient()
+
+const handleIsChat = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {chat_uuid} = req.body
+
+  try {
+    const messages: ChatMessage[] = await prisma.chatMessage.findMany({
+      where: {chat_uuid: chat_uuid}
+    })
+
+    if (messages.length === 0) { // ChatMessage has no data
+      return res.status(200).json({data: false})
+    } else { // Dataset has data
+      return res.status(200).json({data: messages})
+    }
+
+  } catch (e) {
+    return res.status(500).json({message: 'Error connecting to Server.', e})
+  }
+
+}
+
+handler.post(handleIsChat)
+
+export default handler
