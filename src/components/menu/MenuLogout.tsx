cd346dedf56557eefb7efdfa275f481abb409140
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-19 01:30:12 +0800

    feat: remove localStorage keyItem

--- src/components/menu/MenuLogout.tsx
+++ src/components/menu/MenuLogout.tsx
@@ -10,7 +10,10 @@ function MenuLogout() {
   return (
     <div
       className={'flex flex-row rounded-lg mb-1 hover:bg-tuna-900 hover:cursor-pointer'}
-      onClick={() => router.push('/')}
+      onClick={async () => {
+        localStorage.removeItem('open_api_key')
+        await router.push('/')
+      }}
     >
       <LogoutIcon width={16} height={16} className={'ml-2 mt-3 text-wordColor-light'}/>
       <button className={'p-2 text-left text-wordColor-light'}>{t('menu.logout')}</button>
